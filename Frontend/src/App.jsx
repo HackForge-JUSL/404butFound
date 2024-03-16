21d5c04510ca30a4f7e19bdc3476810ca1238fbf
Indrajit Pal <palindrajit10@gmail.com>
2024-03-16 16:02:24 +0530

    login and signup page frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,17 @@
-import { useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
