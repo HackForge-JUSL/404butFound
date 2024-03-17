522133e544b2d4cba19a4ca55bd2871a52facc27
Indrajit Pal <palindrajit10@gmail.com>
2024-03-17 14:05:27 +0530

    twilio changes

--- Twilio/index.js
+++ Twilio/index.js
@@ -3,7 +3,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const twilio = require("twilio");
 
+const cors = require("cors");
+
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 4200;
 
 const { TWILIO_ACCOUNT_SID, YOUR_TWILIO_AUTH_TOKEN } = process.env;
@@ -17,31 +20,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // Route for receiving SMS messages
 app.post("/sms", (req, res) => {
-  // const receivedMessage = req.body.Body;
-  // const senderNumber = req.body.From;
+  const receivedMessage = req.body.Body;
+  const senderNumber = req.body.From;
   console.log(req.body);
 
-  // console.log(`Received message: ${receivedMessage} from ${senderNumber}`);
+  console.log(`Received message: ${receivedMessage} from ${senderNumber}`);
 
-  // // Process the received message and decide on a reply
-  // let replyMessage = "";
-  // if (receivedMessage.toLowerCase().includes("hello")) {
-  //   replyMessage = "Hi there! How can I assist you?";
-  // } else {
-  //   replyMessage = "Sorry, I didn't understand that. Please try again.";
-  // }
+  // Process the received message and decide on a reply
+  let replyMessage = "";
+  if (receivedMessage.toLowerCase().includes("hello")) {
+    replyMessage = "Hi there! How can I assist you?";
+  } else {
+    replyMessage = "Sorry, I didn't understand that. Please try again.";
+  }
 
-  // // Send a reply message
-  // client.messages
-  //   .create({
-  //     body: "Hii",
-  //     to: '+918944015868',
-  //     from: '+16095345886',
-  //   })
-  //   .then((message) => console.log(`Sent reply: ${message.sid}`));
+  // Send a reply message
+  client.messages
+    .create({
+      body: "Hii",
+      to: '+918944015868',
+      from: '+16095345886',
+    })
+    .then((message) => console.log(`Sent reply: ${message.sid}`));
 
-  // res.sendStatus(200);
+  res.sendStatus(200);
 });
+
+app.get('/get-messages', (req, res) => {
+  client.messages.list({ limit: 20 })
+    .then(messages => messages.forEach(m => console.log(m.body)));
+})
 
 // Start the server
 app.listen(port, () => {
