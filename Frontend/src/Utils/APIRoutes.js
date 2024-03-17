3d574468b9b89cb7f581fd8275a0b4946a6b8a44
Indrajit Pal <palindrajit10@gmail.com>
2024-03-17 17:28:17 +0530

    api changes

--- Frontend/src/Utils/APIRoutes.js
+++ Frontend/src/Utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://localhost:3001";
+export const host = "http://localhost:3005";
 
 export const userLoginRoute = `${host}/login`;
 export const userSignupRoute = `${host}/signup`;
